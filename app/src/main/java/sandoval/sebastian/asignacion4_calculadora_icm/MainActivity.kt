3e7b5d220c488dbffe45899750a3af414e4780ad
sebassandoc <sebassandoc@gmail.com>
2025-01-29 20:42:49 -0600

    codigo principal

--- app/src/main/java/sandoval/sebastian/asignacion4_calculadora_icm/MainActivity.kt
+++ app/src/main/java/sandoval/sebastian/asignacion4_calculadora_icm/MainActivity.kt
@@ -2,18 +2,48 @@ package sandoval.sebastian.asignacion4_calculadora_icm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    val height: EditText = findViewById(R.id.height)
-    val weight: EditText = findViewById(R.id.weight)
-    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val height: EditText = findViewById(R.id.height)
+        val weight: EditText = findViewById(R.id.weight)
+        val imc: TextView = findViewById(R.id.imc)
+        val range: TextView = findViewById(R.id.range)
+        val calculate: Button = findViewById(R.id.calculate)
+
+
+        calculate.setOnClickListener {
+            var h = height.text.toString().toDouble()
+            var w = weight.text.toString().toDouble()
+            var res = w/h
+            imc.setText(res.toString())
+            range.setText(calcularIBM(res))
+        }
     }
 
+    fun calcularIBM(valor: Double):String{
+        if(valor<18.5){
+            return "Bajo peso"
+        }else if(valor>=18.5 && valor < 25){
+            return "normal"
+        }else if(valor>=25 && valor < 30){
+            return "Sobrepeso"
+        }else if(valor>=30 && valor < 35){
+            return "Obesidad grado 1"
+        }else if(valor>=35 && valor < 40){
+            return "Obesidad grado 2"
+        }else{
+            return "Obesidad grado 3"
+        }
+
+    }
 
 }
